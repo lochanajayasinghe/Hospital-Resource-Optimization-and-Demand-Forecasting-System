52328624762056919afdf8049f3fb8267f9d7d61
Lochana Jayasinghe <lochanajayasinghe2002@gmail.com>
2025-12-27 02:10:29 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,14 @@
 
 import './App.css'
 import { Outlet } from 'react-router-dom'
-import Navbar from './components/Navbar'
 import MyFooter from './components/MyFooter'
-import Cart from './shop/Cart'
-import { useState } from 'react'
-import BestProducts from './home/BestProducts'
-
 
 function App() {
-
-  const [show,setShow]=useState(true);
-  const [cart,setCart]=useState([]);
-  const handleClick = (product) => {
-    // console.log(product);
-    let isPresent =false;
-
-    cart.forEach((item)=>{
-      if(product._id === item._id)
-        isPresent=true;
-    })
-    if(isPresent)
-        return;
-      setCart ([...cart, product]);
-    } 
-
   return (
-    <>
-  
-    <div className='min-h-screen'> 
-    
-   
-    <Outlet/>
+    <div className='min-h-screen'>
+      <Outlet />
+      <MyFooter />
     </div>
-  
-     <MyFooter/>
-    </>
   )
 }
 
