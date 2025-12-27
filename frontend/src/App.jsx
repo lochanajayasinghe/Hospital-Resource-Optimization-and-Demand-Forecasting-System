3de66e8a07934b2bf3f0cc453fd01069bab5f467
Lochana Jayasinghe <lochanajayasinghe2002@gmail.com>
2025-12-27 16:40:21 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,13 @@
 
 import './App.css'
 import { Outlet } from 'react-router-dom'
-import MyFooter from './components/MyFooter'
+
 
 function App() {
   return (
     <div className='min-h-screen'>
       <Outlet />
-      <MyFooter />
+      
     </div>
   )
 }
